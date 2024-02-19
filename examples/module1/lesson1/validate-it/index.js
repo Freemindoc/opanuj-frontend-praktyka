71082befac589688302ffec4890cb2f9c3990796
Freemindoc <97979394+Freemindoc@users.noreply.github.com>
2024-02-19 20:40:31 +0100

    firstExample

--- examples/module1/lesson1/validate-it/index.js
+++ examples/module1/lesson1/validate-it/index.js
@@ -4,31 +4,24 @@ function validator() {
   const button2 = document.getElementById('button2');
   const result = document.getElementById('result');
 
-  button.addEventListener('click', () => {
-    if (input.value) {
-      if (Number.isInteger(input.value)) {
-        if (
-          Number(input.value) > 0 &&
-          Number(input.value) < 100 &&
-          Number(input.value) % 2 === 0
-        ) {
-          result.innerHTML = 'Valid';
-        } else {
-          result.innerHTML = 'Invalid';
-        }
-        result.innerHTML = 'Valid';
-      } else {
-        result.innerHTML = 'Invalid';
-      }
-    } else {
-      result.innerHTML = 'Invalid';
-    }
-  });
+  button.addEventListener('click', validate);
 
-  button2.addEventListener('click', () => {
-    input.value = '';
-    result.innerHTML = '';
-  });
+  button2.addEventListener('click', clear);
+}
+
+function validate () {
+  if(!input.value) return;
+  result.innerHTML = parseNumber(input.value) > 0 && parseNumber(input.value) < 100 ? "valid" : "invalid";
+}
+
+function parseNumber(inputValue) {
+  const number = Math.round(inputValue);
+  return parseInt(number);
+}
+
+function clear () {
+  input.value = '';
+  result.innerHTML = '';
 }
 
 validator();
